96248af43566ae5b74220e887a37fb662a71813f
avisrivastava254084 <avi.srivastava254084@gmail.com>
2017-06-15 12:49:43 +0100

    Reading Points

---
+++ filterGauss.cpp
@@ -0,0 +1,39 @@
+#include "opencv2/core/core.hpp"
+#include <iostream>
+#include <vector>
+#include <fstream>
+
+using namespace std;
+using namespace cv;
+
+struct dataPoint{
+		Point2d pt;
+		double GaussDensity;
+};
+
+
+int main()
+{
+	ifstream input;
+    string filename;
+    cout << "Enter the file name :" << endl;
+    cin >> filename;
+    input.open(filename.c_str());
+    char ch;
+	dataPoint myPoint;
+    while(input.fail())
+    {
+        input.clear();
+        cout <<"Incorrect filename, please enter again" << endl;
+        cin >> filename ;
+        input.open(filename.c_str()) ;
+    }
+	vector<dataPoint> points;
+	while (input >> myPoint.pt.x >> ch >> myPoint.pt.y)
+    {
+        myPoint.GaussDensity = 1;
+        points.push_back(myPoint);
+        cout<<myPoint.pt.x<<" "<<myPoint.pt.y<<endl;
+    };     
+	return 0;
+}
