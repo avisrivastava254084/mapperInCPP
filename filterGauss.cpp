218480fb06ae90d36b71d6962fdad02cdc05f999
Ayush Mishra <mishraayush945@gmail.com>
2017-06-17 00:21:45 +0530

    Eliminating "ch" variable.
    Adding WINPAUSE function to pause console wherever we want to.

--- filterGauss.cpp
+++ filterGauss.cpp
@@ -8,46 +8,50 @@
 
 #define w 400
 
+#ifdef _WIN32
+#define WINPAUSE system("pause")
+#endif
+
 using namespace std;
 using namespace cv;
 
-struct dataPoint{
-		Point2d pt;
-		double GaussDensity;
+struct dataPoint {
+	Point2d pt;
+	double GaussDensity;
 };
 
 int main()
 {
-	char window[]="Plotting Points";
+	char window[] = "Plotting Points";
 	cv::Mat mapperImage;
-	mapperImage = cv::Mat::zeros(w,w,CV_8UC3);
+	mapperImage = cv::Mat::zeros(w, w, CV_8UC3);
 	ifstream input;
-    string filename;
-    cout << "Enter the file name :" << endl;
-    cin >> filename;
-    input.open(filename.c_str());
-    char ch;
-    
-    Point2d center;
+	string filename;
+	cout << "Enter the file name :" << endl;
+	cin >> filename;
+	input.open(filename.c_str());
+	char ch;
+
+	Point2d center;
 	dataPoint myPoint;
-    while(input.fail())
-    {
-        input.clear();
-        cout <<"Incorrect filename, please enter again" << endl;
-        cin >> filename ;
-        input.open(filename.c_str()) ;
-    }
+	while (input.fail())
+	{
+		input.clear();
+		cout << "Incorrect filename, please enter again" << endl;
+		cin >> filename;
+		input.open(filename.c_str());
+	}
 	vector<dataPoint> points;
-	while (input >> myPoint.pt.x >> ch >> myPoint.pt.y)
-    {
-        myPoint.GaussDensity = 1;
-        points.push_back(myPoint);
-        cout<<myPoint.pt.x<<" "<<myPoint.pt.y<<endl;
-        center = Point(myPoint.pt.x,myPoint.pt.x);
-		circle(mapperImage, center, 1, CV_RGB(255,0,0), 3);
-		
-    }
-    imshow(window, mapperImage);
-	waitKey(10000000);
-	return 0;
-}	
+	while (input >> myPoint.pt.x >> myPoint.pt.y)
+	{
+		myPoint.GaussDensity = 1;
+		points.push_back(myPoint);
+		cout << myPoint.pt.x << " " << myPoint.pt.y << endl;
+		center = Point(myPoint.pt.x, myPoint.pt.x);
+	circle(mapperImage, center, 1, CV_RGB(255, 0, 0), 3);
+
+}
+imshow(window, mapperImage);
+WINPAUSE;
+return 0;
+}
