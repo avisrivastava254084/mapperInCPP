a652e8c8cf13bcb5aacb2a37179a40231032e30a
Aviral Srivastava <avi.srivastava254084@gmail.com>
2017-07-06 00:23:58 +0100

    Optimising with fewer "ifs"
    The cost of iterating once and adding all the required edges was too many conditions, it has been improved. 2 conditions with two times iteration.

--- squareGrid.cpp
+++ squareGrid.cpp
@@ -18,90 +18,43 @@ struct VertexProperty{
 
 typedef boost::property<boost::edge_weight_t, double> EdgeProperty;
 typedef boost::adjacency_list<boost::vecS, boost::vecS, boost::undirectedS, VertexProperty, EdgeProperty> Graph;
-typedef boost::graph_traits<Graph>::vertex_iterator VItr;
-typedef boost::graph_traits<Graph>::edge_iterator EItr;
-typedef boost::graph_traits<Graph>::adjacency_iterator adjacency_iterator;
-typedef boost::property_map<Graph, boost::vertex_index_t>::type IndexMap;
 typedef boost::graph_traits<Graph>::edge_iterator EdgeIterator;
 typedef std::pair<EdgeIterator, EdgeIterator> EdgePair;
-typedef boost::graph_traits<Graph>::vertex_descriptor VertexDescriptor;
-typedef vector<pair< pair< Point2d, Point2d>, double> > EdgeLinear;
 
-void write_image(String const& name, Mat const& mapperImage) {
-  try {
-    cv::imwrite(name, mapperImage);
-  } catch (std::runtime_error& ex) {
-    fprintf(stderr, "Error in writing the image\n", ex.what());
-  }
-}
-
-void euclideanVertices(){
-	int m, n, i, j;
-	int vertexName = 65;
-	VItr vitr, vend;
-	cout<<"Enter the m and n:"<<endl;
-	cin >> m >> n;
-	m++; n++;
-	Graph g(m*n);
-	boost::tie(vitr, vend) = boost::vertices(g);
-	cout<<"And the points are:"<<endl;
-	for( i = 0; i <= m; i++){
-		for( j = 0; j <= n; j++){
-			g[*vitr].pt.x = i;
-			g[*vitr].pt.y = j;
-			g[*vitr].name = vertexName;
-			++vertexName;
-			cout << g[*vitr].pt.x <<" "<< g[*vitr].pt.y << endl;
-			++vitr;
+void euclideanVertices(Graph& g, int m, int n) {
+	int a = 0;
+	for(int i = 0; i <= m; i++) {
+		for(int j = 0; j <= n; j++) {
+			g[a].pt.x = i;
+			g[a].pt.y = j;
+			g[a].name = a;
+			a++;
 		}
 	}
-	cout<<"Testing the indices of the graph"<<endl;
-	tie(vitr, vend) = vertices(g);
-	int lengthLimit = m-1; int breadthLimit = m * (n-1);
-	cout << "The values of limits are:" << lengthLimit << " " << breadthLimit << endl;
-	for(int i = 0; i < (m*n); i++){
-		if( i == lengthLimit && i != breadthLimit ){
-			if( (i+m) <= (m*n) ){
-				add_edge(i, (i+m), 1, g);
-				cout << "LengthLimit An edge is added between the vertices:" << i << " " << (i+m) << endl;
-			}
-			lengthLimit += m;
-			continue;
-		}
-		if( i == breadthLimit && i != lengthLimit ){
-			if( (i+1) <= (m*n) ){
-				add_edge(i, (i+1), 1, g);
-				cout << "BreadthLimit An edge is added between the vertices:" << i << " " << (i+1) << endl;
-			}
-			breadthLimit++;
-			continue;
-		}
-		if( i == ((m*n) - 1) ){
-			break;
-		}
-		else{
-			if( (i+1) <= (m*n) ){
-				add_edge(i, (i+1), 1, g);
-				cout << "Else first An edge is added between the vertices:" << i << " " << (i+1) << endl;
-			}
-			if( (i+m) <= (m*n) ){
-				add_edge(i, (i+m), 1, g);
-				cout << "Else second An edge is added between the vertices:" << i << " " << (i+m) << endl;
-			}
-		}
-	}
-	EdgePair ep; Point2d vertexOne, vertexTwo;
-	VertexDescriptor u, v;
-	/*for(ep = edges(g); ep.first != ep.second; ++ep.first){
-		u = source(*ep.first,g); vertexOne = g[u].pt;
-		v = target(*ep.first,g); vertexTwo = g[v].pt;
-		cout << "An edge exists between:" << u <<" "<< v <<endl;
-	}*/
 }
-	//write_image("squareGrid.png", mapperImage);
 
+void addEdges(Graph& g, int m, int n) {
+	int i, j;
+	for (i = 0; i < (( m + 1 ) * ( n )); i++) { 
+		add_edge(i, i+(m+1), g);
+		cout<<endl<<"An edge has been added in:"<<i<<" "<<i+m+1<<endl;
+	}
+	int breadthLimit = m;
+	for (i = 0; i < ((m+1) * (n+1) - 1); i++) {
+		if ( i == breadthLimit) {
+			breadthLimit += (m+1);
+			continue;
+		}
+		add_edge(i, (i + 1), g);
+		cout<<endl<<"An edge has been added in:"<<i<<" "<<i+1<<endl;
+	}
+}
 
 int main(){
-	euclideanVertices();
-	return 0;
+	cout << "Enter the m and n:" << endl;
+	int m, n;
+	cin >> m >> n;
+	Graph g((m+1)*(n+1));
+	euclideanVertices(g, m, n);
+	addEdges(g, m, n);
 }
