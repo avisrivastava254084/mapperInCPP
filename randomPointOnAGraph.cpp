9fe2ef852ea5dcd0cd0528f0288d969eacbd1ec7
Aviral Srivastava <avi.srivastava254084@gmail.com>
2017-07-12 16:03:31 +0100

    All work done
    Boost Graph generated and scaled;
    Random Cloud generated on the above boost graph;
    Above cloud generated as a graph and scaled;
    Neighbourhood graph for the aforementioned cloud(it's graph) generated and scaled.

--- randomPointOnAGraph.cpp
+++ randomPointOnAGraph.cpp
@@ -8,6 +8,8 @@
 #include <opencv2/highgui/highgui.hpp>
 #include <math.h>
 
+#define big_constant 1e+64
+
 using namespace std;
 using namespace cv;
 
@@ -26,6 +28,7 @@ typedef boost::graph_traits<Graph>::edge_iterator EdgeIterator;
 typedef std::pair<EdgeIterator, EdgeIterator> EdgePair;
 typedef boost::graph_traits<Graph>::vertex_descriptor VertexDescriptor;
 typedef vector<pair< pair< Point2d, Point2d>, double> > EdgeLinear;
+typedef vector<Point2d> VectorPoint2d;
 
 double distanceBetweenTwoPoints(Point2d pt1, Point2d pt2){
 	double distanceX = (pt1.x-pt2.x)*(pt1.x-pt2.x);
@@ -43,19 +46,20 @@ void write_image(String const& name, Mat const& mapperImage) {
 }
 
 double Rand() { return rand() % 100; }
-double RandUnity() { return (double) rand() / RAND_MAX ; }
+double RandUnity() { return (double)  rand() / RAND_MAX; }
 
 bool sortByEdgelength(const pair<pair<Point2d, Point2d>, double> &a, const pair<pair<Point2d, Point2d>, double> &b){
 	return (a.second < b.second);
 }
 
-void randomPoint(const double& totalLength, vector<double>& edgesLinearInterval, EdgeLinear& edgesLinear, Graph& g, Mat const& mapperImage, vector<Point2d>& RandomPoints){
-	double linearPoint = RandUnity() * totalLength; double t;
+void randomPoint(const double& totalLength, vector<double>& edgesLinearInterval, EdgeLinear& edgesLinear, Graph& g, Mat const& mapperImage, VectorPoint2d& RandomPoints){
+	double linearPoint, t; int noOfEdge = 0; int check = 0;
+	linearPoint = RandUnity() * totalLength;
 	if(linearPoint == 0) { linearPoint += RandUnity(); }
-	cout<<endl<<"The random linear point is:"<<linearPoint<<endl; int noOfEdge = 0;
-	int check = 0 ;
+	cout<<endl<<"The random linear point is:"<<linearPoint<<endl; noOfEdge = 0;
+	check = 0 ;
 	for(vector<double>::iterator it = edgesLinearInterval.begin(); it != edgesLinearInterval.end(); ++it){
-		if((*it) > (linearPoint)){
+		if((*it) > (linearPoint)) {
 			cout<<endl<<"the value at it is:"<<(*it)<<"and at the prev pos is:"<<(*(it-1))<<endl;
 			cout<<"The denominator is:"<< ((*it) - (*(it-1)))<<endl;
 			t = ((linearPoint - (*(it-1))) / ((*it) - (*(it-1))) );
@@ -72,13 +76,13 @@ void randomPoint(const double& totalLength, vector<double>& edgesLinearInterval,
 	cout<<"Now, iterate through the edges' lengths."<<endl; 
 	for(ep = edges(g); ep.first != ep.second; ++ep.first){
 		if(trackOfEdge == noOfEdge){
-			cout<<endl<<"Hi"<<endl;
+			cout<<endl<<"reached the interval"<<endl;
 			u = source(*ep.first,g); vertexOne = g[u].pt;
 			v = target(*ep.first,g); vertexTwo = g[v].pt;
 			break;
 		}
 		else{
-			cout<<endl<<"it is OK hi"<<endl;
+			cout<<endl<<"not reached the interval yet"<<endl;
 			trackOfEdge++;
 		}
 	}
@@ -86,29 +90,47 @@ void randomPoint(const double& totalLength, vector<double>& edgesLinearInterval,
 	plottedPoint.x = ((1-t) * g[u].pt.x) + ((t) * g[v].pt.x); cout<<"The x is:"<<plottedPoint.x<<endl;
 	plottedPoint.y = ((1-t) * g[u].pt.y) + ((t) * g[v].pt.y); cout<<"The y is:"<<plottedPoint.y<<endl;
 	RandomPoints.push_back(plottedPoint);
-	circle(mapperImage, plottedPoint, 1, CV_RGB(0,0,0), 3);
-    write_image("randomPoint.png", mapperImage);
+	//circle(mapperImage, plottedPoint, 1, CV_RGB(0,0,0), 3);
+    //write_image("randomPoint.png", mapperImage);
 }	
 
-void euclideanGraph(Graph& g, vector<Point2d>& RandomPoints, int randomCloudSize) {
-  Mat mapperImage = Mat::zeros(600, 600, CV_8UC3);
-  mapperImage = cv::Scalar(255,255,255);
+void euclideanCloud(Graph& cloud, VectorPoint2d RandomPoints, int randomCloudSize, Mat& cloudImage) {
+	cout<<endl<<"Here we are assigning the random cloud its actual coords:"<<endl;
+	VItr vitr, vend;
+	tie(vitr, vend) = boost::vertices(cloud);
+	for (vector<Point2d>::iterator it = RandomPoints.begin(); ((vitr != vend) && (it != RandomPoints.end())); ++vitr, ++it) {
+		Point2d pt = *it;
+		cloud[*vitr].pt.x = pt.x;
+		cloud[*vitr].pt.y = pt.y;
+		//centre = Point(g[*vitr].pt.x, g[*vitr].pt.y);
+		//circle(mapperImage, centre, 1, CV_RGB(255,0,0), 6);
+		cout << "The coordinates are:" << cloud[*vitr].pt.x << " " << cloud[*vitr].pt.y << endl;
+	}
+}
+
+void euclideanGraph(Graph& g, VectorPoint2d& RandomPoints, int randomCloudSize, Mat& mapperImage) {
+  srand(time(NULL));
   VItr vitr, vend;
   vector<double> edgesLinearInterval;
   srand(time(NULL));
   Point2d vertexOne, vertexTwo, centre;
   EdgeLinear edgesLinear;
   double length, totalLength; totalLength = 0;
-  boost::add_edge(0, 1, g);
-  boost::add_edge(1, 2, g);
-  boost::add_edge(2, 3, g);
-  boost::add_edge(3, 1, g);
+  cout<<"Now, keep on adding edges, when done, enter -1."<<endl;
+	int check; int i, j; check = 0;
+	while(check != -1) {
+		cout << endl << "Enter the two vertices which you want to connect through an edge"<<endl;
+		cin >> i >> j;
+		add_edge(i, j, g);
+		cout << endl<<"Enter -1 if you do not want to add any more edge(s)"<<endl;
+		cin >> check;
+	}
   boost::tie(vitr, vend) = boost::vertices(g);
   for (; vitr != vend; ++vitr) {
-    g[*vitr].pt.x = Rand();
-    g[*vitr].pt.y = Rand();
-    centre = Point(g[*vitr].pt.x, g[*vitr].pt.y);
-    circle(mapperImage, centre, 1, CV_RGB(255,0,0), 6);
+    g[*vitr].pt.x = RandUnity();
+    g[*vitr].pt.y = RandUnity();
+    //centre = Point(g[*vitr].pt.x, g[*vitr].pt.y);
+    //circle(mapperImage, centre, 1, CV_RGB(255,0,0), 6);
     cout << "The coordinates are:" << g[*vitr].pt.x << " " << g[*vitr].pt.y << endl;
   }
   EdgePair ep;
@@ -119,7 +141,7 @@ void euclideanGraph(Graph& g, vector<Point2d>& RandomPoints, int randomCloudSize
 	  u=source(*ep.first,g); vertexOne = g[u].pt;
 	  v=target(*ep.first,g); vertexTwo = g[v].pt;
 	  length = distanceBetweenTwoPoints(vertexOne, vertexTwo);
-	  line(mapperImage, vertexOne, vertexTwo, CV_RGB(0,0,255));
+	  //line(mapperImage, vertexOne, vertexTwo, CV_RGB(0,0,255));
 	  cout<<length<<" ";
 	  edgesLinear.push_back(make_pair(make_pair(vertexOne, vertexTwo), length));
   }
@@ -150,9 +172,110 @@ void euclideanGraph(Graph& g, vector<Point2d>& RandomPoints, int randomCloudSize
   }
 }
 
+void Bounding_Box_Graph (Graph const& graph, std::pair<Point2d, Point2d>& graph_box ) {
+	graph_box.first = Point2d( big_constant, big_constant );
+	graph_box.second = Point2d( -big_constant, -big_constant );
+	VItr vitr, vend;
+	boost::tie(vitr, vend) =  boost::vertices(graph);
+	for (; vitr != vend; ++vitr ) {
+		if ( graph_box.first.x > graph[*vitr].pt.x ) graph_box.first.x = graph[*vitr].pt.x;
+		if ( graph_box.first.y > graph[*vitr].pt.y ) graph_box.first.y = graph[*vitr].pt.y;
+		if ( graph_box.second.x < graph[*vitr].pt.x ) graph_box.second.x = graph[*vitr].pt.x;
+		if ( graph_box.second.y < graph[*vitr].pt.y ) graph_box.second.y = graph[*vitr].pt.y;
+	}
+}
+
+void Bounding_Box_Cloud (Graph const& cloud, std::pair<Point2d, Point2d>& cloud_box ) {
+	cloud_box.first = Point2d( big_constant, big_constant );
+	cloud_box.second = Point2d( -big_constant, -big_constant );
+	VItr vitr, vend;
+	boost:tie(vitr, vend) = boost::vertices(cloud);
+	for (; vitr != vend; ++vitr ) {
+		if ( cloud_box.first.x > cloud[*vitr].pt.x ) cloud_box.first.x = cloud[*vitr].pt.x;
+		if ( cloud_box.first.y > cloud[*vitr].pt.y ) cloud_box.first.y = cloud[*vitr].pt.y;
+		if ( cloud_box.second.x < cloud[*vitr].pt.x ) cloud_box.second.x = cloud[*vitr].pt.x;
+		if ( cloud_box.second.y < cloud[*vitr].pt.y ) cloud_box.second.y = cloud[*vitr].pt.y;
+	}
+}
+
+void Scaling_Parameters_Graph (Graph const& graph, Point image_sizes, double& scale, Point2d& shift) {
+	std::pair<Point2d, Point2d> box;
+	Bounding_Box_Graph( graph, box );
+	Point2d graph_sizes( box.second.x - box.first.x, box.second.y - box.first.y );
+	Point2d graph_center( box.second.x + box.first.x, box.second.y + box.first.y );
+	graph_center /= 2;
+	Point2d image_center( image_sizes.x / 2, image_sizes.y / 2 );
+	scale = std::min( image_sizes.x / graph_sizes.x, image_sizes.y / graph_sizes.y );
+	// transformation: point -> scale * ( point - cloud_center ) + image_center
+	shift = image_center - scale * graph_center;
+}
+
+void Scaling_Parameters_Cloud (Graph const& cloud, Point image_sizes, double& scale, Point2d& shift) {
+	std::pair<Point2d, Point2d> box;
+	Bounding_Box_Cloud( cloud, box );
+	Point2d cloud_sizes( box.second.x - box.first.x, box.second.y - box.first.y );
+	Point2d cloud_center( box.second.x + box.first.x, box.second.y + box.first.y );
+	cloud_center /= 2;
+	Point2d image_center( image_sizes.x / 2, image_sizes.y / 2 );
+	scale = std::min( image_sizes.x / cloud_sizes.x, cloud_sizes.y / cloud_sizes.y );
+	// transformation: point -> scale * ( point - cloud_center ) + image_center
+	shift = image_center - scale * cloud_center;
+}
+
+// Draw a scaled cloud to the image of a given size
+
+void Draw_Graph (Graph const& graph, int radius, Scalar color, Mat& image, double& scale, Point2d& shift, String name) {
+	cout<<endl<<"Now, we will be plotting the given boost graph:"<<endl;
+	Scaling_Parameters_Graph( graph, image.size(), scale, shift );
+	VItr vitr, vend;
+	boost:tie(vitr, vend) = boost::vertices(graph);
+	VertexDescriptor u,v; EdgePair ep; Point2d vertexOne, vertexTwo;
+	for(ep = edges(graph); ep.first != ep.second; ++ep.first) {
+		u = source(*ep.first, graph); vertexOne = scale * graph[u].pt + shift;
+		v = target(*ep.first, graph); vertexTwo = scale * graph[v].pt + shift;
+		cout<<endl<<"A line will be plotted on these:"<<vertexOne<<" "<<vertexTwo<<endl;
+		line(image, vertexOne, vertexTwo, CV_RGB(0,0,255));
+	}
+	for (; vitr != vend; ++vitr) {
+		cout<<endl<<"This will be the circle plotted vertices' indices wise:"<<graph[*vitr].pt<<endl;
+		cout<<endl<<"Vertex plotted at:"<<(scale * graph[*vitr].pt + shift)<<endl;
+		circle( image, Point( scale * graph[*vitr].pt + shift ), radius, color, -1 );
+	}
+	write_image(name, image);
+}
+
+void neighbourHoodGraph(Graph& randomGraph, double threshold) {
+	VItr vitr, vend; double distance;
+	tie(vitr, vend) = vertices(randomGraph);
+	VItr vitr2, vend2;
+	tie(vitr2, vend2) = vertices(randomGraph);
+	for(; vitr != vend; ++vitr) {
+		for(vitr2 = vitr+1; vitr2 != vend2; ++vitr2) {
+			distance = distanceBetweenTwoPoints(randomGraph[*vitr].pt, randomGraph[*vitr2].pt);
+			cout<<endl<<"The distance between " << randomGraph[*vitr].pt << " " << randomGraph[*vitr2].pt << " is: " << distance << endl;
+			if(distance <= threshold) {
+				add_edge(*vitr, *vitr2, randomGraph);
+				cout << endl << "An edge has been added to the vertices : " << randomGraph[*vitr].pt << " and " << randomGraph[*vitr2].pt << endl;
+			}
+		}
+	}
+}
+
 int main() {
-    Graph g(4);
-    vector<Point2d> RandomPoints;
+    cout<<endl<<"Please enter the total number of vertices in the boost graph:"<<endl;
+    int numberOfVertices; cin >> numberOfVertices;
+    Graph g(numberOfVertices);
+    VectorPoint2d RandomPoints;
+    Mat cloudImage = Mat::zeros(600, 600, CV_8UC3); cloudImage = cv::Scalar(255,255,255);
+    Mat graphImage = Mat::zeros(600, 600, CV_8UC3); graphImage = cv::Scalar(255,255,255);
     int randomCloudSize; cout << endl << "Please enter the size of the random cloud that you want to generate" << endl; cin >> randomCloudSize;
-    euclideanGraph(g, RandomPoints, randomCloudSize);
-  }
+    Graph randomGraph(randomCloudSize);
+    euclideanGraph(g, RandomPoints, randomCloudSize, graphImage);
+    euclideanCloud(randomGraph, RandomPoints, randomCloudSize, cloudImage);
+    double scaleGraph; Point2d shiftGraph;
+    Draw_Graph(g, 4, CV_RGB(255, 0, 0), graphImage, scaleGraph, shiftGraph, "scaledBoostGraph.png");
+    cout<<endl<<"Please enter the threshold:"<<endl;
+    double threshold; cin >> threshold;
+    neighbourHoodGraph(randomGraph, threshold);
+    Draw_Graph(randomGraph, 4, CV_RGB(255, 0, 0), cloudImage, scaleGraph, shiftGraph, "scaledRandomCloudGraph.png");
+}
